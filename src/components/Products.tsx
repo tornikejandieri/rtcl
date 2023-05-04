27543b8abd239b519b185ff448954e309c2fc5fe
formyauto75@gmail.com <formyauto75@gmail.com>
2023-05-04 11:00:14 +0400

    feat: optimizations and typesafety

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
 import { addToCart } from '../models/cart/cartSlice'
 import { HiOutlineShoppingCart } from 'react-icons/hi'
+import { useAppDispatch } from '../hooks'
+import { products } from '../data'
 
 export interface Product {
   name: string,
@@ -11,15 +11,7 @@ export interface Product {
 }
 
 const Products = () => {
-  const [products, setProducts] = useState<Product[]>([
-    {name: 'shoe', price: 500, id: 1, quantity: 0},
-    {name: 'dress', price: 1000, id: 2, quantity: 0},
-    {name: 'hat', price: 300, id: 3, quantity: 0},
-    {name: 'watch', price: 2000, id: 4, quantity: 0},
-    {name: 'tshirt', price: 100, id: 5, quantity: 0},
-])
-
-const dispatch = useDispatch()
+const dispatch = useAppDispatch()
 
   return (
     <div>
---
+++ src/data/index.ts
@@ -0,0 +1,7 @@
+export const products = [
+  { name: "shoe", price: 500, id: 1, quantity: 0 },
+  { name: "dress", price: 1000, id: 2, quantity: 0 },
+  { name: "hat", price: 300, id: 3, quantity: 0 },
+  { name: "watch", price: 2000, id: 4, quantity: 0 },
+  { name: "tshirt", price: 100, id: 5, quantity: 0 },
+] as const
